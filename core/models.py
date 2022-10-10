ecd07ee637ee75bb10dd2f18dd4bb7a6cf82a141
Ismael de Laet Abashi <ismael.abashi@skyone.solutions>
2022-10-10 15:31:10 -0300

    Create models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    member_name = models.CharField('Name', max_length=100)
+
+    def __str__(self):
+        return self.member_name
+
+
+class Objective(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    objective_text = models.CharField('Objective', max_length=150)
+
+    def __str__(self):
+        return self.objective_text
+
+
+class KeyResult(models.Model):
+    objective = models.ForeignKey(Objective, on_delete=models.CASCADE)
+    kr_text = models.CharField('Key Result', max_length=200, blank=True)
+
+    def __str__(self):
+        return self.kr_text
