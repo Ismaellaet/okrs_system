aa781da60e353cd39033e521f7d318f06680f01d
Ismael de Laet Abashi <ismael.abashi@skyone.solutions>
2022-10-27 15:16:07 -0300

    Add OKR in django-admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, OKR
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('id', 'member_name')
+    list_display = ('id', 'name')
+
+
+@admin.register(OKR)
+class OKRAdmin(admin.ModelAdmin):
+    list_display = ('member', 'objective', 'key_result_1', 'key_result_2', 'key_result_3', 'key_result_4',
+                    'key_result_5')
